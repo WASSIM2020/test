00d8ad16ca839625baa747c3e838abe4c1070186
Wassim Ghanmi <61029860+WASSIM2020@users.noreply.github.com>
2025-12-10 14:45:15 +0100

    Update Runner.java

--- src/test/java/run/Runner.java
+++ src/test/java/run/Runner.java
@@ -1,20 +1,22 @@
 package run;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-		tags="  @Ajoutproduit",
-		features="src/test/features",
-		glue={"hooks","stepDefinition"},
-		publish=true,
-		plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber.json","junit:target/cucumber.xml","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} 
-		
-		)
+    tags = "@Ajoutproduit",                     // ← sans espace devant !
+    features = "src/test/features",
+    glue = {"hooks", "stepDefinition"},
+    publish = true,
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber.json",
+        "junit:target/cucumber.xml",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    }
+)
 public class Runner {
-
 }
